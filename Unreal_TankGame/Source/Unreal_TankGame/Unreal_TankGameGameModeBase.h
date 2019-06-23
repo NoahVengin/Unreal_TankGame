128475a58f07e9917d7a3825c9b100476d9a589b
Noah Vengin <noahvengin@yahoo.com>
2019-06-23 17:47:58 -0400

    Basic Setup

---
+++ Unreal_TankGame/Source/Unreal_TankGame/Unreal_TankGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Unreal_TankGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_TANKGAME_API AUnreal_TankGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
